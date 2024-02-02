7ab05330111eb1e2655077fb09e158816a121653
Aleksandra Połacik <polacik.ola@gmail.com>
2024-02-02 08:37:27 +0100

    del

--- CalculatorApp/src/calculatorapp/NewJFrame.java
+++ CalculatorApp/src/calculatorapp/NewJFrame.java
@@ -1,9 +1,5 @@
 package calculatorapp;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
 /**
  *
